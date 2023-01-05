9c72df494ab77d50943f00eb2e5f5d8d4d9d3eb7
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-01-05 17:03:37 +0000

    declaração dos metodos da classe Program em biblioteca.cs

--- biblioteca.cs
+++ biblioteca.cs
@@ -1,7 +1,21 @@
 using System;
 using Modelo;
 class Program {
-  public static void Main () {
-    
-  }
+  public static void Main () {}
+  public static void Menu() {}
+  public static void LivroInserir() {}
+  public static void LivroAtualizar() {}
+  public static void LivroApagar() {}
+  public static void LivroListar() {}
+  public static void ExemplarInserir() {}
+  public static void ExemplarAtualizar() {}
+  public static void ExemplarApagar() {}
+  public static void ExemplarListar() {}
+  public static void LocadorInserir() {}
+  public static void LocadorAtualizar() {}
+  public static void LocadorApagar() {}
+  public static void LocadorListar() {}
+  public static void Alugar() {}
+  public static void Devolver() {}
+  
 }
