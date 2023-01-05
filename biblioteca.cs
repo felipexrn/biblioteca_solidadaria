9898891ea3160652d55f37c1164a2689fc656ccc
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-01-05 17:18:43 +0000

    criação do namespace visao no arquivo biblioteca.cs

--- biblioteca.cs
+++ biblioteca.cs
@@ -1,23 +1,24 @@
 using System;
 using Modelo;
-class Biblioteca {
-  public static void Main () {}
-  public static void Menu() {}
-  public static void LivroInserir() {}
-  public static void LivroAtualizar() {}
-  public static void LivroApagar() {}
-  public static void LivroListar() {}
-  public static void ExemplarInserir() {}
-  public static void ExemplarAtualizar() {}
-  public static void ExemplarApagar() {}
-  public static void ExemplarListar() {}
-  public static void LocacaoInserir() {}
-  public static void LocacaoAtualizar() {}
-  public static void LocacaoApagar() {}
-  public static void LocacaoListar() {}
-  public static void LocadorInserir() {}
-  public static void LocadorAtualizar() {}
-  public static void LocadorApagar() {}
-  public static void LocadorListar() {}
-  
+namespace visao {
+  class Biblioteca {
+    public static void Main () {}
+    public static void Menu() {}
+    public static void LivroInserir() {}
+    public static void LivroAtualizar() {}
+    public static void LivroApagar() {}
+    public static void LivroListar() {}
+    public static void ExemplarInserir() {}
+    public static void ExemplarAtualizar() {}
+    public static void ExemplarApagar() {}
+    public static void ExemplarListar() {}
+    public static void LocacaoInserir() {}
+    public static void LocacaoAtualizar() {}
+    public static void LocacaoApagar() {}
+    public static void LocacaoListar() {}
+    public static void LocadorInserir() {}
+    public static void LocadorAtualizar() {}
+    public static void LocadorApagar() {}
+    public static void LocadorListar() {}
+  }
 }
