862d0215b711e9f75aa83a80797e5417ab566262
José Elton <115085872+eltonsilva23@users.noreply.github.com>
2023-01-16 13:32:23 +0000

    class Locacao

--- Locacao.cs
+++ Locacao.cs
@@ -1,6 +1,10 @@
 using System;
 namespace Modelo {
   public class Locacao {
-    
+    public int Id {get,set}
+    public DateTime DataLocacao {get,set}
+    public int IdExemplar {get,set}
+    public int IdLocador {get,set}
+    private DataTime dataDevolucao
   }
 }
