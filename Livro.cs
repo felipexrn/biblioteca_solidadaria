b0565b43a552a45f3a81024127169a11d3dd989c
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-01-16 13:31:12 +0000

    interface IComparalble classe Livro

--- Livro.cs
+++ Livro.cs
@@ -1,6 +1,6 @@
 using System;
 namespace Modelo {
-  public class Livro {
+  public class Livro : IComparable {
     public int Id {get, set}
     public string Titulo {get, set} /*implementar*/
     public string Autor {get, set} /*implementar*/
@@ -9,5 +9,9 @@ namespace Modelo {
     public Livro() {}
     public Livro(string t, string a, string d, string i) {/*implementar*/}
     public override string ToString() {/*implementar*/}
+    public int CompareTo(object obj) {
+      Livro l2 = (Livro) obj;
+      return this.Titulo.CompareTo(l2.Titulo);
+    }
   }
 }
