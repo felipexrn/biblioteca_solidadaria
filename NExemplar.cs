44b768e166e268fdc79167f02cb9b7b229172281
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-01-23 12:33:40 +0000

    estrutura das classes NExemplar e NLivros feita

--- NExemplar.cs
+++ NExemplar.cs
@@ -1,6 +1,12 @@
 using System;
+using Modelo;
+using System.Collections.Generic;
 namespace Negocio {
   public class NExemplar {
-    
+    private static List<Exemplar> exemplares;
+    public static void ExemplarInserir();
+    public static void ExemplarAtualizar();
+    public static void ExemplarExcluir();
+    public static void ExemplarListar();
   }
 }
--- NLivro.cs
+++ NLivro.cs
@@ -1,6 +1,12 @@
 using System;
+using Modelo;
+using System.Collections.Generic;
 namespace Negocio {
   public class NLivro {
-    
+    private static List<Livro> livros;
+    public static void LivroInserir();
+    public static void LivroAtualizar();
+    public static void LivroExcluir();
+    public static void LivroListar();
   }
 }
