518a81970c438d75a461c81ceb397fe6ca9d057b
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-01-25 10:56:41 +0000

    Adição de construtores com parâmetros na classe Exemplar

--- Exemplar.cs
+++ Exemplar.cs
@@ -5,7 +5,15 @@ namespace Modelo {
     public bool Alugado {get; set;} /*implementar*/
     public int IdLivro {get; set;} /*implementar*/
     public Exemplar() {}
-    public Exemplar(bool a, int idLivro) {/*implementar*/}
+    public Exemplar(int id, bool a, int idLivro) {
+      Id = id;
+      Alugado = a;
+      IdLivro = idLivro;
+    }
+    public Exemplar(bool a, int idLivro) {
+      Alugado = a;
+      IdLivro = idLivro;
+    }
     public override string ToString() {
       return $"Id: {this.Id}\nAlugado: {this.Alugado}\nIdLivro: {this.IdLivro}";  
     }
