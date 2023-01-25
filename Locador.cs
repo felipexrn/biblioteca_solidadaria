f30665058ae38631f3b4e65a82ae4d6c71539909
José Elton <115085872+eltonsilva23@users.noreply.github.com>
2023-01-25 13:09:10 +0000

    Alteração em Locador colocação dos construtores

--- Locador.cs
+++ Locador.cs
@@ -4,5 +4,18 @@ namespace Modelo {
     public int Id{get;set;}
     public string Nome{get;set;}
     public string Telefone{get;set;}
+    public Locador (){}
+    public Locador(int id, string n, string t, int idLocacao) {
+      Id = id;
+      Nome = n;
+      Telefone = t;
+    }
+    public Locador(string n, string t){
+      Nome = n;
+      Telefone = t;
+    }
+    public overside string ToString(){
+      return $"Id: {this.Id}\nNome: {this.Nome}\nTelefone: {this.Telefone}";
+    }
   }
 }
