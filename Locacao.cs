2230ff1169daf6c69e18e69f39091926c3ce80c0
José Elton <115085872+eltonsilva23@users.noreply.github.com>
2023-01-26 20:46:10 +0000

    Alteração no arquivo Locacao com a inserção dos construtores

--- Locacao.cs
+++ Locacao.cs
@@ -5,6 +5,21 @@ namespace Modelo {
     public DateTime DataLocacao {get;set;}
     public int IdExemplar {get;set;}
     public int IdLocador {get;set;}
-    private DateTime dataDevolucao;
+    private DateTime dataDevolucao;//Perguntar si inclui no construtor ToString
+    public Locacao(){}
+    public Locacao(int id, DateTime dataLoc, int idExemplar,int idLocador){
+      Id = id;
+      DataLocacao = dataLoc;
+      IdExemplar = idExemplar;
+      IdLocador = idLocador;
+    }
+    public Locacao (DateTime dataLoc, int idExemplar, int idLocador){
+      DataLocacao = dataLoc;
+      IdExemplar = idExemplar;
+      IdLocador = idLocador;
+    }
+    public override string ToString(){
+      return $"Id: {this.Id}\nData de Locacao: {this.DataLocacao}\nId Exemplar: {this.IdExemplar}\nId Locador: {this.IdLocador}";
+    }
   }
 }
