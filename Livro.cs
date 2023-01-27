0515308c6749d7d8fedf8974980c48520297eb4b
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-01-27 20:10:37 +0000

    Inserção das propriedades Volume e Edicao na classe Livro

--- Livro.cs
+++ Livro.cs
@@ -4,25 +4,31 @@ namespace Modelo {
   public class Livro : IComparable {
     public int Id {get; set;} /*implementar*/
     public string Titulo {get; set;} /*implementar*/
+    public string Volume {get; set;} /*implementar*/
+    public string Edicao {get; set;} /*implementar*/
     public string Autor {get; set;} /*implementar*/
     public string Descricao {get; set;} /*implementar*/
     public string Isbn {get; set;} /*implementar*/
     public Livro() {}
-    public Livro(int id, string t, string a, string d, string i) {
+    public Livro(int id, string t, sring v, string e, string a, string d, string i) {
       Id = id;
       Titulo = t;
+      Volume = v;
+      Edicao = e;
       Autor = a;
       Descricao = d;
       Isbn = i;
     }
-    public Livro(string t, string a, string d, string i) {
+    public Livro(string t, sring v, string e, string a, string d, string i) {
       Titulo = t;
+      Volume = v;
+      Edicao = e;
       Autor = a;
       Descricao = d;
       Isbn = i;
     }
     public override string ToString() {
-      return $"Id: {this.Id}\nTitulo: {this.Titulo}\nAutor: {this.Autor}\nDescricao: {this.Descricao}\nIsbn: {this.Isbn}";
+      return $"Id: {this.Id}\nTitulo: {this.Titulo}\nVolume: {this.Volume}\nEdição: {this.Edicao}\nAutor: {this.Autor}\nDescricao: {this.Descricao}\nIsbn: {this.Isbn}";
     }
     public int CompareTo(object obj) {
       Livro l2 = (Livro) obj;
